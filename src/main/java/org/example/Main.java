65dbf237d55db1fa5eb29cc0b1bae4047b11d75d
Francisco Maia <franciscomaia097@gmail.com>
2023-12-21 16:15:20 +0000

    Added obterQuociente function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,19 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+    }
+
+    //Funçao que devolve quociente
+    public static double obterQuociente(double dividendo, double divisor) {
+        if (divisor != 0) {
+            return dividendo / divisor;
+        } else {
+            // Tratamento para divisão por zero
+            System.out.println("Erro: Divisão por zero não é permitida.");
+            return Double.NaN; // NaN (Not a Number) pode ser usado para indicar um valor indefinido ou não representável
+        }
     }
 }

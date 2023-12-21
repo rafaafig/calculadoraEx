08905441b5acc5853fee3a759964fdcf469a164e
Nuno Silva <nuno_2011_20@hotmail.com>
2023-12-21 16:17:46 +0000

    Added function exercice 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public String questao6(){
+        return "O VALOR DA QUESTAO 5 a somar algo";
+    }
 }

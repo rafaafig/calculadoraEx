c77fd910652fc040dc393e4cf303dfc52471a7b7
Francisco Maia <franciscomaia097@gmail.com>
2023-12-21 16:03:31 +0000

    Added class calculator

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Calculadora {
+
+
+    /*
+    1. Obter a soma de dois números;
+
+    2. Obter o produto de dois números;
+
+    3. Obter a subtração de dois números;
+
+    4. Obter o quociente de dois números;
+
+    5. Memorizar um resultado de uma operação;
+
+    6. Reaproveitar o valor memorizado no âmbito de uma operação;
+
+    7. Apagar (limpar) o valor memorizado
+
+    8. Calcular xy
+
+    9. Calcular o fatorial de um número
+    
+    */
+
+}

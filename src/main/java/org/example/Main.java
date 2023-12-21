3873afd3b21d483d1226eb80c1ccba4d4b34d30e
Rafa <rafa.figueiredo48@gmail.com>
2023-12-21 16:11:37 +0000

    Added Soma function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static int soma(int[] numeros) {
+        int soma = 0;
+
+        for (int numero : numeros) {
+            soma += numero;
+        }
+
+        return soma;
+    }
 }
